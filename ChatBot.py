282067cf23eed6ff57683dcc4fe9a9660bd22344
KenjiIT <128443526+KenjiIT@users.noreply.github.com>
2023-04-24 09:03:31 +0700

    fix chat

--- ChatBot.py
+++ ChatBot.py
@@ -1,26 +1,25 @@
-from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper, ServiceContext
-from langchain import OpenAI
+from llama_index import SimpleDirectoryReader, GPTSimpleVectorIndex, LLMPredictor, PromptHelper, ServiceContext
 from langchain.chat_models import ChatOpenAI
 import sys
 import os
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 
 def construct_index(directory_path):
     # set maximum input size
-    max_input_size = 4096
+    max_input_size = 500
     # set number of output tokens
     num_outputs = 4096
     # set maximum chunk overlap
     max_chunk_overlap = 20
     # set chunk size limit
-    chunk_size_limit = 600 
+    chunk_size_limit = 600
 
     # define prompt helper
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
 
     # define LLM
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
- 
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.5, model_name="gpt-3.5-turbo", max_tokens=num_outputs, top_p=1,frequency_penalty=0, presence_penalty=0.6))
+
     documents = SimpleDirectoryReader(directory_path).load_data()
     
     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
@@ -44,7 +43,7 @@ def ask_ai():
         print(Fore.YELLOW + Style.BRIGHT + Style.NORMAL +"Chatbot: " + response.response)
 
 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 
 construct_index("data")
 ask_ai()
