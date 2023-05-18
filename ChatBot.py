cadee62cc63d95df102a704ce596ff11e0c93b6a
KenjiIT <128443526+KenjiIT@users.noreply.github.com>
2023-05-18 20:11:37 +0700

    fix bot
    fix xong box

--- ChatBot.py
+++ ChatBot.py
@@ -1,36 +1,40 @@
-from llama_index import SimpleDirectoryReader, GPTSimpleVectorIndex, LLMPredictor, PromptHelper, ServiceContext
+from llama_index import StorageContext,SimpleDirectoryReader, GPTVectorStoreIndex,SimpleDirectoryReader, load_index_from_storage, LLMPredictor, PromptHelper, ServiceContext
 from langchain.chat_models import ChatOpenAI
-import sys
 import os
 from colorama import Fore, Style
 
+
 def construct_index(directory_path):
     # set maximum input size
-    max_input_size = 500
+    max_input_size = 4096
     # set number of output tokens
-    num_outputs = 4096
+    num_outputs = 2000
     # set maximum chunk overlap
     max_chunk_overlap = 20
     # set chunk size limit
-    chunk_size_limit = 600
+    chunk_size_limit = 600 
 
     # define prompt helper
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
 
     # define LLM
-    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.5, model_name="gpt-3.5-turbo", max_tokens=num_outputs, top_p=1,frequency_penalty=0, presence_penalty=0.6))
-
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.5, model_name="text-davinci-003", max_tokens=num_outputs))
+ 
     documents = SimpleDirectoryReader(directory_path).load_data()
     
     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-    index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+    index = GPTVectorStoreIndex.from_documents(documents,service_context=service_context)
 
-    index.save_to_disk('index.json')
-
+    index.set_index_id("vector_index")
+    index.storage_context.persist('storage')
+   
     return index
 
 def ask_ai():
-    index = GPTSimpleVectorIndex.load_from_disk('index.json')
+    # rebuild storage context
+    storage_context = StorageContext.from_defaults(persist_dir='storage')
+    # load index
+    index = load_index_from_storage(storage_context, index_id="vector_index")
     print(
             Fore.GREEN
             + Style.BRIGHT
@@ -38,12 +42,14 @@ def ask_ai():
             + Fore.GREEN + Style.BRIGHT + "ChatBot: Tôi có thể giúp gì được cho bạn?"
         )
     while True: 
-        query =  input(Fore.WHITE + Style.BRIGHT +"User: ")
-        response = index.query(query)
-        print(Fore.YELLOW + Style.BRIGHT + Style.NORMAL +"Chatbot: " + response.response)
+        query_engine = index.as_query_engine()
+        query =  input(Fore.BLUE + Style.BRIGHT +"User: ")
+        response = query_engine.query(query)
+        print(Fore.LIGHTMAGENTA_EX + Style.BRIGHT + "Chatbot: " + response.response)
+        
 
 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 
 construct_index("data")
 ask_ai()
